d133849d966eae220eb41ffce99dd89b02e8c9fb
pps-admin <pps.github@gmail.com>
2026-02-10 17:09:37 +0900

    Update: path 'generator/generator_manual.py' - Updated by 'hea_'

--- generator/generator_manual.py
+++ generator/generator_manual.py
@@ -3,6 +3,14 @@ import sys
 t = int(sys.argv[1])
 
 if t == 1:
-    print('1 2')
+    print('arO O O O O O')
 elif t == 2:
-    print('3 7')
+    print('XrO X O X O X')
+elif t == 3:
+    print('△ △')
+elif t == 4:
+    print('O O')
+elif t == 5:
+    print('X X')
+elif t == 6:
+    print('arOX△ △ O X X')
