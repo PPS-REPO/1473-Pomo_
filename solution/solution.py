435af17374d8a90d064d59ee3a9399db69701f4d
pps-admin <pps.github@gmail.com>
2026-02-13 15:22:30 +0900

    Update: path 'solution/solution.py' - Updated by 'hea_'

--- solution/solution.py
+++ solution/solution.py
@@ -1,2 +1,2 @@
 x = list(input().split(' '))    
-print(x[0], x[1:].count('O') + x[1:].count('△') + x[1:].count('X'))
+print(x[0], x[1:].count('O') * 3 + x[1:].count('△') * 2 + x[1:].count('X') * 1)
